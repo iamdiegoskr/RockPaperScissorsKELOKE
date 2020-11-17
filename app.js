17150b0a69788631355983b400afb7bb3a1d5a1a
diego martinez <juandiego337@hotmail.com>
2020-11-17 15:00:08 -0500

    add eventListener to options, add function getComputerChoise to get a random option,

--- app.js
+++ app.js
@@ -10,3 +10,34 @@ const result_Div = document.querySelector('.result');
 const optionRockDiv = document.getElementById('rock');
 const optionPaperDiv = document.getElementById('paper');
 const optionScissorDiv = document.getElementById('scissor');
+
+
+function getComputerChoise() {
+    const choises = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * choises.length);
+    return choises[randomNumber];
+}
+
+function game(userChoice) {
+
+    const computerChoise = getComputerChoise();
+
+}
+
+function main() {
+
+    optionRockDiv.addEventListener('click', function() {
+        game('r')
+    })
+
+    optionPaperDiv.addEventListener('click', function() {
+        game('p')
+    })
+
+    optionScissorDiv.addEventListener('click', function() {
+        game('s')
+    })
+
+}
+
+main();
