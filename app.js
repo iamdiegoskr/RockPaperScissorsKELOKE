c7238690bc5df23c48c335910e7e8958ed0ea716
diego martinez <juandiego337@hotmail.com>
2020-11-17 00:28:05 -0500

    we add variables for the project

---
+++ app.js
@@ -0,0 +1,12 @@
+const userScore = 0;
+const computerScore = 0;
+
+//DOM variables
+const userScore_Span = document.getElementById('userScore');
+const computerScore_Span = document.getElementById('computerScore');
+const scoreBoard_Div = document.querySelector('.score-board');
+const result_Div = document.querySelector('.result');
+
+const optionRockDiv = document.getElementById('rock');
+const optionPaperDiv = document.getElementById('paper');
+const optionScissorDiv = document.getElementById('scissor');
